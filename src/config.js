1a0040a5c3c6149751aca27b929a16e183768ca7
Kkkermit <jeaton446@gmail.com>
2024-05-01 14:51:22 +0100

    add: Added info folder and fixed imports

--- src/config.js
+++ src/config.js
@@ -3,6 +3,8 @@ module.exports = {
     status: "dnd",
     eventListeners: 100,
     embedColor: "Blurple",
-    dev: "Kkermit"
+    dev: "Kkermit",
+    devBy: "| Developed by kkermit",
+    noPermsMessage: `You **do not** have the permission to do that!`,
 
 }
