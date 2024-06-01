ce1eda624ed9f1bbc339280ca97d4b017a6fcebb
Kkkermit <jeaton446@gmail.com>
2024-06-01 20:44:42 +0100

    add: Added mongoDB connection

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,11 +1,37 @@
 const config = require('../config');
 const fs = require('fs');
 const path = require('path');
+const mongoose = require('mongoose');
+const mongodbURL = process.env.mongodb;
 
 module.exports = {
     name: 'ready',
     once: true,
     async execute(client) {
+
+        client.logs.info(`[SCHEMAS] Started loading schemas...`);
+
+        if (!mongodbURL) return;
+
+        mongoose.set("strictQuery", false);
+        await mongoose.connect(mongodbURL || ``, {
+            keepAlive: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+
+        if (mongoose.connect) {
+            client.logs.success('[DATABASE] Connected to MongoDB successfully.')
+
+            const schemaFolder = path.join(__dirname, '../schemas'); 
+            fs.readdir(schemaFolder, (err, files) => {
+                if (err) {
+                    client.logs.error('[ERROR] Error reading schemas folder:', err);
+                    return;
+                }
+                client.logs.success(`[SCHEMAS] Loaded ${files.length} schema files.`);
+            });
+        }
 
         client.logs.logging(`[BOT] ${client.user.username} has been launched!`);
         client.logs.info(`[EVENTS] Started loading events...`)
@@ -14,7 +40,7 @@ module.exports = {
         const triggerFolder = path.join(__dirname, '../triggers'); 
         fs.readdir(triggerFolder, (err, files) => {
             if (err) {
-                client.logs.error('[TRIGGERS] Error reading trigger folder:', err);
+                client.logs.error('Error reading trigger folder:', err);
                 return;
             }
             client.logs.info(`[TRIGGERS] Started loading triggers...`);
---
+++ src/schemas/schemasTemplateFile.js
@@ -0,0 +1,3 @@
+// This is a template file for creating a new schema file.
+// It's here so it doesn't reads the first file and doesn't throw an error.
+// You can delete this file and replace it with an actual schema file.
