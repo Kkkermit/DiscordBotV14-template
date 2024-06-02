0a6cc0f81a30dfd77531300a561dad05dc081869
Kkkermit <jeaton446@gmail.com>
2024-06-02 19:15:41 +0100

    add: Added error handling on disallowed intents

--- src/index.js
+++ src/index.js
@@ -7,38 +7,38 @@ const fs = require('fs');
 
 const currentVersion = "v2.0.2";
 
-const client = new Client({ intents: [
-    GatewayIntentBits.Guilds, 
-	GatewayIntentBits.GuildMessages, 
-	GatewayIntentBits.MessageContent, 
-	GatewayIntentBits.GuildMembers, 
-	GatewayIntentBits.GuildPresences, 
-	GatewayIntentBits.GuildIntegrations, 
-	GatewayIntentBits.GuildWebhooks, 
-    GatewayIntentBits.GuildMessageReactions,
-	GatewayIntentBits.MessageContent, 
-	GatewayIntentBits.GuildEmojisAndStickers, 
-	GatewayIntentBits.DirectMessages, 
-	GatewayIntentBits.DirectMessageTyping, 
-	GatewayIntentBits.GuildModeration, 
-	GatewayIntentBits.GuildVoiceStates,
-	GatewayIntentBits.GuildWebhooks, 
-	GatewayIntentBits.AutoModerationConfiguration,
-	GatewayIntentBits.GuildScheduledEvents, 
-	GatewayIntentBits.GuildMessageTyping, 
-	GatewayIntentBits.AutoModerationExecution, 
-],  
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.MessageContent, 
+        GatewayIntentBits.GuildMembers, 
+        GatewayIntentBits.GuildPresences, 
+        GatewayIntentBits.GuildIntegrations, 
+        GatewayIntentBits.GuildWebhooks, 
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.MessageContent, 
+        GatewayIntentBits.GuildEmojisAndStickers, 
+        GatewayIntentBits.DirectMessages, 
+        GatewayIntentBits.DirectMessageTyping, 
+        GatewayIntentBits.GuildModeration, 
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildWebhooks, 
+        GatewayIntentBits.AutoModerationConfiguration,
+        GatewayIntentBits.GuildScheduledEvents, 
+        GatewayIntentBits.GuildMessageTyping, 
+        GatewayIntentBits.AutoModerationExecution, 
+    ],  
 
-partials: [
-    Partials.GuildMember, 
-    Partials.Channel,
-    Partials.GuildScheduledEvent,
-    Partials.Message,
-    Partials.Reaction, 
-    Partials.ThreadMember, 
-    Partials.User
-]
-
+    partials: [
+        Partials.GuildMember, 
+        Partials.Channel,
+        Partials.GuildScheduledEvent,
+        Partials.Message,
+        Partials.Reaction, 
+        Partials.ThreadMember, 
+        Partials.User
+    ]
 }); 
 
 client.logs = require('./utils/logs');
@@ -110,7 +110,7 @@ const commandFolders = fs.readdirSync("./src/commands");
     client.login(process.env.token).then(() => {
         checkVersion(currentVersion);
     }).catch((error) => {
-        client.logs.error(`[LOGIN] Error while logging in. Check if your token is correct.`);
+        console.error(`${color.red}[${getTimestamp()}]${color.reset} [LOGIN] Error while logging in. Check if your token is correct or double check your also using the correct intents. \n${color.red}[${getTimestamp()}]${color.reset} [LOGIN]`, error);
     });
 })();
 
