21f91d5e778ca28850825dac78e9f628bd81b923
Kkkermit <jeaton446@gmail.com>
2024-06-02 03:35:10 +0100

    add: Added more error handling to messageCreate file

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -11,6 +11,26 @@ module.exports = {
     if (!message.content.startsWith(client.config.prefix)) return;
         const args = message.content.slice(client.config.prefix.length).trim().split(/ +/);
 
+    const color = {
+        red: '\x1b[31m',
+        orange: '\x1b[38;5;202m',
+        yellow: '\x1b[33m',
+        green: '\x1b[32m',
+        blue: '\x1b[34m',
+        reset: '\x1b[0m'
+    }
+
+    function getTimestamp() {
+        const date = new Date();
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        const seconds = date.getSeconds();
+        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    }
+
     let cmd = args.shift().toLowerCase();
         if (cmd.length === 0) return;
 
@@ -40,7 +60,7 @@ module.exports = {
         try {
             command.execute(message, client, args);
         } catch (error) {
-            client.logs.error(error)
+            console.error(`${color.red}[${getTimestamp()}] [MESSAGE_CREATE] Error while executing command. \n${color.red}[${getTimestamp()}] [MESSAGE_CREATE] Please check you are using the correct execute method: "async execute(message, client, args)":`, error);
 
             const embed = new EmbedBuilder()
             .setColor("Red")
