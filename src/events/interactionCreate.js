a25afa899239d4a59e58e13d2f251fe9682fcf7d
Kkkermit <jeaton446@gmail.com>
2024-06-02 03:32:46 +0100

    add: Added error handling to interactionCreate

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -5,6 +5,26 @@ module.exports = {
     async execute(interaction, client) {
         if (!interaction.isCommand()) return;
 
+        const color = {
+            red: '\x1b[31m',
+            orange: '\x1b[38;5;202m',
+            yellow: '\x1b[33m',
+            green: '\x1b[32m',
+            blue: '\x1b[34m',
+            reset: '\x1b[0m'
+        }
+
+        function getTimestamp() {
+            const date = new Date();
+            const year = date.getFullYear();
+            const month = date.getMonth() + 1;
+            const day = date.getDate();
+            const hours = date.getHours();
+            const minutes = date.getMinutes();
+            const seconds = date.getSeconds();
+            return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+        }
+
         const command = client.commands.get(interaction.commandName);
 
         if (!command) return
@@ -13,7 +33,7 @@ module.exports = {
             await command.execute(interaction, client);
         } catch (error) {
 
-            client.logs.error(error);
+            console.error(`${color.red}[${getTimestamp()}] [INTERACTION_CREATE] Error while executing command. \n${color.red}[${getTimestamp()}] [INTERACTION_CREATE] Please check you are using the correct execute method: "async execute(interaction, client)":`, error);
 
             const embed = new EmbedBuilder()
             .setColor("Red")
